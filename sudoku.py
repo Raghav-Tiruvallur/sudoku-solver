6caaed05060169cc346e663a00ba48044c687a85
Raghav Tiruvallur <rtiruvallur@gmail.com>
2021-09-25 13:42:31 +0530

    added findEmptySpotsFunction

--- sudoku.py
+++ sudoku.py
@@ -21,4 +21,10 @@ def displayBoard(board):
                 print(board[i][j])
             else:
                 print(str(board[i][j])+" ",end=" ")
-displayBoard(board)
+
+def findEmptySpots(board):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j]==0:
+                return (i,j)
+    return None
